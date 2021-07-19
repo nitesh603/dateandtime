287264764a275cd8224a4271da019d9850a3f2c6
nitesh603 <77894579+nitesh603@users.noreply.github.com>
2021-07-19 10:32:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let date = new Date();
+            document.body.innerHTML = "<p> Today the date is " + date + "</p>";
+            let name = prompt("What is your name?")
+            alert("Hey, " + name + "!");
+            
+
+
+
